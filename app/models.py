e13d8523391bb4d18c31d61091e809334d2492cb
Kir <kir-sth@yandex.ru>
2024-06-11 15:05:15 +0300

    change models

--- app/models.py
+++ app/models.py
@@ -1,66 +1,66 @@
-from sqlalchemy import Column, ForeignKey, JSON
-from sqlalchemy.orm import relationship
-from sqlalchemy.types import Integer, String, Float, Boolean
+from sqlalchemy import ForeignKey, JSON
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+
 from app.database import Base
+
 
 class Forms(Base):
     __tablename__ = 'forms'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    tg_user_id = Column(Integer)
-    group_id = Column(Integer, ForeignKey("groups.id"))
-    name = Column(String)
-    age = Column(Integer)
-    gender = Column(String)
-    description = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    city = Column(String)
-    quad_key = Column(Integer)
-    photo_ids = Column(JSON)
-    preference_id = Column(Integer, ForeignKey("preferences.id"))
-    preference = relationship("Preferences", back_populates="forms")
-    min_age = Column(Integer)
-    max_age = Column(Integer)
-    location_radius = Column(Float)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    tg_user_id: Mapped[int]
+    group_id: Mapped[int] = mapped_column(ForeignKey("groups.id"))
+    name: Mapped[str]
+    age: Mapped[int]
+    gender: Mapped[str]
+    description: Mapped[str]
+    latitude: Mapped[float]
+    longitude: Mapped[float]
+    city: Mapped[str]
+    quad_key: Mapped[int]
+    photo_ids: Mapped[list[str]] = mapped_column(JSON)
+    preference_id: Mapped[int] = mapped_column(ForeignKey("preferences.id"))
+    min_age: Mapped[int]
+    max_age: Mapped[int]
+    location_radius: Mapped[float]
 
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    tg_user_id = Column(Integer)
-    name = Column(String)
-    age = Column(Integer)
-    gender = Column(String)
-    description = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    city = Column(String)
-    quad_key = Column(Integer)
-    photo_ids = Column(JSON)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    tg_user_id: Mapped[int]
+    name: Mapped[str]
+    age: Mapped[int]
+    gender: Mapped[str]
+    description: Mapped[str]
+    latitude: Mapped[float]
+    longitude: Mapped[float]
+    city: Mapped[str]
+    quad_key : Mapped[int]
+    photo_ids: Mapped[list[str]] = mapped_column(JSON)
 
 class Groups(Base):
     __tablename__ = 'groups'
 
-    id = Column(Integer, primary_key=True, index=True)
-    description = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    description: Mapped[Optional[str]]
 
 class Likes(Base):
     __tablename__ = 'likes'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id_from = Column(Integer, ForeignKey('users.id'))
-    user_id_to = Column(Integer, ForeignKey('users.id'))
-    is_match = Column(Boolean)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id_from: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user_id_to: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    is_match: Mapped[bool]
 
 class Preferences(Base):
     __tablename__ = 'preferences'
     
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    preferred_gender = Column(String)
-    min_age = Column(Integer)
-    max_age = Column(Integer)
-    location_radius = Column(Float)
-    forms = relationship("Forms", back_populates="preference")
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    preferred_gender: Mapped[str]
+    min_age: Mapped[int]
+    max_age: Mapped[int]
+    location_radius: Mapped[float]
